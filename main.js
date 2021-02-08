ad7b3aeab7f97aff8ab76f78054da8f88164723b
slubert <oliver.tallgard@gmail.com>
2021-02-08 13:46:31 +0200

    update test

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 const midBox = document.getElementById("midBox")
+const fiveRecentBox = document.getElementById('fiveRecentBox')
 const fiveRecentList = document.getElementById('fiveRecentList')
 const currentAverageTime = document.getElementById("currentAverageTime");
-const output = document.getElementById("output");
+const info = document.getElementById('infoBox')
+const startBox = document.getElementById('startBox')
 const bestSingleTimeBox = document.getElementById("bestSingleTimeBox");
 const bestSingleTime = document.getElementById("bestSingleTime");
-const button = document.getElementById("startButton");
 const bestAverageTimeBox = document.getElementById("bestAverageTimeBox");
 const bestAverageTime = document.getElementById("bestAverageTime");
 
-window.addEventListener('touchstart', onHoldDown);
-window.addEventListener('touchend', onHoldUp)
-
-
-
-output.style.display = "none"
 let counting = false;
 let timeStop
 let timeStart
 boxAmount = 5
 let times = []
 let sumOfAvridge
+let isMobile
+
+//check if the phone is flipped and relodes the sight if so 
+window.addEventListener("orientationchange", function() {
+    window.location.reload();
+}, false);
 
 //grabs the best times from local storage
 let bestTimes = JSON.parse(localStorage.getItem('bestTimes'))
@@ -33,32 +34,52 @@ else{
     bestAverageTime.textContent = bestTimes.average + 's'
 }
 
-function mobileCheck() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
-};
-
-
+// checks if user is on mobile or not
 if (mobileCheck()){
+    //if mobile
+    isMobile = true;
     alert('phone')
+
+    //checks so the orgentation is vertical if not asks you to flip your phone 
+    if (window.innerWidth < window.innerHeight){
+        hide();
+        info.innerHTML = 'flip your phone 🔄'
+    }
+    else{
+        window.addEventListener('touchstart', onHoldDown);
+        window.addEventListener('touchend', onHoldUp)
+
+        info.textContent = 'plase 6 fingers on the screen to start'
+    }
+}
+else{
+    //if pc
+    alert('pc')
+    window.addEventListener('keyup', onHoldDown);
+    window.addEventListener('keydown', onHoldUp)
+
+    info.textContent = 'hold \'A\' \'S\' \'D\' \'J\' \'K\' \'L\' to start'
 }
 
 
-function onHoldDown(e){
-    if (e.touches.length > 6){
-        alert('more then six')
+function onHoldDown(event){
+    console.log(event.keyCode)
+    if (checkIfFingersAreOnScreen(event) || checkIfKeysAreDown(event)){
         if (!counting){
             hide()
-            
-            output.style.display = null
-            output.textContent = 'relise to start'
-            button.textContent = null
-            button.style.backgroundColor = 'white'
+    
+            info.textContent = 'relice to start'
         }
         else{
             console.log('stop');
             timeStop = new Date()
+    
+            if (isMobile){
+                info.textContent = 'plase 6 fingers on the screen to start'
+            }
+            else{
+                info.textContent = 'hold \'A\' \'S\' \'D\' \'J\' \'K\' \'L\' to start'
+            }
     
             //checks if you have played 5 times
             if (times.length >= boxAmount){
@@ -75,9 +96,6 @@ function onHoldDown(e){
             setToRcentFive(timeCalculate());
     
             show()
-            button.textContent = 'HOLD'
-            button.style.backgroundColor = "rgb(9, 255, 0)"
-            output.style.display = 'none'
         }
     }
 }
@@ -86,12 +104,8 @@ function onHoldUp(e){
     if (!counting){
         console.log('start');
         timeStart = new Date()
-
-        output.textContent = "SOLVE"
-        button.textContent = 'press to stop'
-        button.style.backgroundColor = 'red'
         counting = true
-
+        info.textContent = 'solve'
     }
     else{
         counting = false;
@@ -121,6 +135,18 @@ function setToRcentFive(time){
 }
 
 
+function checkIfFingersAreOnScreen(event){
+    if(event.touches.length > 6){
+        return(true)
+    }
+}
+
+function checkIfKeysAreDown(event){
+    if(event.keyCode == 65 && event.keyCode == 68 && event.keyCode == 74 && event.keyCode == 75 && event.keyCode == 76 && event.keyCode == 83 ){
+        return(true)
+    }
+}
+
 //calculate time function
 function timeCalculate(){
     finalTime = (timeStop.getTime() - timeStart.getTime()) / 1000
@@ -139,23 +165,24 @@ function checkBestTime(){
     }
 }
 
-
+function mobileCheck() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
 
 
 //show and hide functions
 function hide(){
-    fiveRecentList.style.display = "none"
-    currentAverageTime.style.display = "none"
-    bestAverageTimeBox.style.display = "none"
-    bestSingleTimeBox.style.display = "none"
+    fiveRecentBox.style.display = 'none'
+    startBox.style.display = 'none'
+    info.style.fontSize = '3rem'
 }
 function show(){
-    fiveRecentList.style.display = null
-    currentAverageTime.style.display = null
-    bestAverageTimeBox.style.display = null
-    bestSingleTimeBox.style.display = null
+    fiveRecentBox.style.display = null
+    startBox.style.display = null
+    info.style.fontSize = '2rem'
 }  
-
 
 
 // button.textContent = e.touches.length
